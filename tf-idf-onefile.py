b98d56671645f83fe6be29f73dc8f1029b5f5150
Eric Bair <47259344+ericbair@users.noreply.github.com>
2021-03-14 14:09:11 -0700

    Update tf-idf-onefile.py
    - Prints top 3 weights for each song
    - We can eyeball the lists and make qualitative judgements

--- tf-idf-onefile.py
+++ tf-idf-onefile.py
@@ -1,25 +1,18 @@
-import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas
 
-"""
-import glob
-album_filenames = glob.glob('C:\\Users\\Eric\\Desktop\\data\\comm_success\\*.txt') 
-
-lyrics_as_list_of_strings = []
-
-for album in album_filenames:
-    album_open = open(album, 'r', errors='ignore')  
-    album_read = album_open.read()
-    lyrics_as_list_of_strings.append(album_read)
-"""
-
-#'/home/dfer/project/commercially_successful_group_all_lyrics.txt'
+# Read lyrics file, separate each song by using split
+# with the divider as an argument.
 lyrics_file = 'C:\\Users\\Eric\\Desktop\\data\\commercially_successful_group_all_lyrics.txt'
 lyrics_file_open = open(lyrics_file, 'r', errors='ignore')       
 corpus = lyrics_file_open.read()
 corpus = corpus.split('###############################################')
 
+# Convert the collection of raw documents into a matrix
+# of TF-IDF features.
+# Strips any accented characters, removes English stop words,
+# tokenizes the text, uses add-one smoothing, and looks
+# only at ngrams of n = 1.
 vectorizer = TfidfVectorizer(input='content',
                              strip_accents='ascii', 
                              analyzer='word', 
@@ -30,16 +23,37 @@ vectorizer = TfidfVectorizer(input='content',
                              ngram_range=(1,1)
                              )
 
+# Fits the corpus to the vectorizer to create 
+# the feature matrix
 tfidf_vectors = vectorizer.fit_transform(corpus)
 
-tfidf_output = tfidf_vectors[0]
+# Display the dimensions on the matrix
+# (x, y) = x songs, y token types (features)
+print("Dimensions of feature matrix: ", tfidf_vectors.shape)
+
+# We want the features for each song, so
+# save how many there are
+number_of_songs = tfidf_vectors.shape[0]
+
+# Need the feature names as well
 feature_names = vectorizer.get_feature_names()
 
-df = pandas.DataFrame(tfidf_output.T.todense(), 
-                  index=feature_names, 
-                  columns=['idf_weights']
-                  )
+for i in range(number_of_songs):
 
-df = df.sort_values(by=['idf_weights'], ascending=False)
+    # Find the vector for the current song
+    tfidf_output = tfidf_vectors[i]
 
-print (df.head(10))
+    # Create a dataframe
+    # x = feature name
+    # y = TFIDF weights
+    df = pandas.DataFrame(tfidf_output.T.todense(), 
+                    index=feature_names, 
+                    columns=['tfidf_weights']
+                    )
+
+    # Sort in descending order so highest weights are
+    # at the top of the column.
+    df = df.sort_values(by=['tfidf_weights'], ascending=False)
+
+    # Print top 3 highest weights for each song
+    print(df.head(3))
